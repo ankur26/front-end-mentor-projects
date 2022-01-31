27cb81a1a16dcba0519eda0b18826a7c0a57e470
Ankur Bhatkalkar <ankur26@users.noreply.github.com>
2022-01-31 10:58:16 +0530

    Added Desktop Nav menu

--- sunnyside-agency-landing-page-main/src/index.js
+++ sunnyside-agency-landing-page-main/src/index.js
@@ -1,14 +1,12 @@
-// window.addEventListener('resize',(e)=>{
-//     console.log(e.target.innerHeight,e.target.innerWidth);
-// })
-
 let hamburger = document.getElementById('hamburger');
 let headerContent = document.querySelector('.header-content');
-let menu = document.querySelector('.menu');
+let menuMobile = document.querySelector('.menu-mobile');
+
+
 // console.log(hamburger);
 // console.log(headerContent);
 // console.log(menu);
 hamburger.addEventListener('click',()=>{
     headerContent.classList.toggle('none');
-    menu.classList.toggle('none');
+    menuMobile.classList.toggle('none');
 })
